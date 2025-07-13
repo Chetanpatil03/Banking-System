c069c6823d2ec6530b1739302962beb5a9c5e608
Chetan Patil <chetanpatil6303@gmail.com>
2025-07-14 01:01:41 +0530

    update BankingApp class and added database credential's

--- src/Banking_App.java
+++ src/Banking_App.java
@@ -1,4 +1,11 @@
 public class Banking_App {
+
+    private static String url = "jdbc:mysql://localhost:3306/Banking_sys";
+    private static String user = "root";
+    private static String pass = "root";
+
+
+
     public static void main(String[] args) {
         System.out.println("Main class and main function");
 
