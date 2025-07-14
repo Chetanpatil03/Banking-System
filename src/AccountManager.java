dcb5486933ba43acb64f4b1f3930067f85501b4e
Chetan Patil <chetanpatil6303@gmail.com>
2025-07-14 19:06:10 +0530

    created transfer_money method to add functionality of transfering the money to another account

--- src/AccountManager.java
+++ src/AccountManager.java
@@ -144,4 +144,76 @@ public class AccountManager {
             e.printStackTrace();
         }
     }
+
+    public void transfer_money(long sender_AccNumber) throws SQLException{
+        sc.nextLine();
+        System.out.print("Enter Receiver Account number : ");
+        long receiver_accNo = sc.nextLong();
+        System.out.print("Enter Amount : ");
+        double amount = sc.nextDouble();
+        sc.nextLine();
+        System.out.print("Enter Security PIN : ");
+        String security_pin = sc.nextLine();
+
+        try {
+            conn.setAutoCommit(false);
+
+            if (sender_AccNumber != 0 && receiver_accNo !=0){
+
+                PreparedStatement preparedStatement = conn.prepareStatement("SELECT * FROM accounts where acc_number = ? and security_pin = ?");
+                preparedStatement.setLong(1,sender_AccNumber);
+                preparedStatement.setString(2,security_pin);
+
+                ResultSet resultSet = preparedStatement.executeQuery();
+
+                if (resultSet.next()){
+                    double current_balance = resultSet.getDouble("balance");
+
+                    if (amount <= current_balance){
+                        String debit_query = "UPDATE accounts SET balance = balance - ? where acc_number = ?";
+                        String credit_query = "UPDATE accounts SET balance = balance + ? where acc_number = ?";
+
+                        PreparedStatement debitPreparedStatement = conn.prepareStatement(debit_query);
+                        debitPreparedStatement.setDouble(1,amount);
+                        debitPreparedStatement.setLong(2,sender_AccNumber);
+
+                        PreparedStatement creditPreparedStatement = conn.prepareStatement(credit_query);
+                        creditPreparedStatement.setDouble(1,amount);
+                        creditPreparedStatement.setLong(2,receiver_accNo);
+
+                        int rowAffected1 = debitPreparedStatement.executeUpdate();
+                        int rowAffected2 = creditPreparedStatement.executeUpdate();
+
+                        if (rowAffected2 > 0 && rowAffected1 > 0){
+                            System.out.println("Transaction successful!");
+                            System.out.println("Rs."+amount+" transferred Successfully to "+sender_AccNumber);
+                            conn.commit();
+                            conn.setAutoCommit(true);
+                            return;
+                        }
+                        else {
+                            System.out.println("Transaction FAILED.......");
+                            conn.rollback();
+                            conn.setAutoCommit(true);
+                        }
+                    }
+                    else {
+                        System.out.println("Insufficient Balance.....");
+                    }
+                }
+                else {
+                    System.out.println("Invalid Security PIN........");
+                }
+
+            }
+            else {
+                System.out.println("Invalid account number !............");
+            }
+
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+        }
+        conn.setAutoCommit(true);
+    }
 }
