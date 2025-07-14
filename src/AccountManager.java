ee1e80ffc6370ed414f4fa6d866f45dbbbfce6ac
Chetan Patil <chetanpatil6303@gmail.com>
2025-07-14 18:39:04 +0530

    created credit_money method which is used to do credit money in bank account

--- src/AccountManager.java
+++ src/AccountManager.java
@@ -68,4 +68,56 @@ public class AccountManager {
         }
         conn.setAutoCommit(true);
     }
+
+    public void credit_money(long acc_number) throws SQLException{
+        sc.nextLine();
+        System.out.print("Enter amount : ");
+        double amount = sc.nextDouble();
+        sc.nextLine();
+
+        System.out.print("Enter security PIN : ");
+        String security_pin = sc.nextLine();
+
+        try{
+            conn.setAutoCommit(false);
+
+            if (acc_number != 0){
+                PreparedStatement preparedStatement = conn.prepareStatement("select * from accounts where acc_number = ? and security_pin = ?");
+                preparedStatement.setLong(1,acc_number);
+                preparedStatement.setString(1,security_pin);
+
+                ResultSet resultSet = preparedStatement.executeQuery();
+
+                if (resultSet.next()){
+
+                    String credit_query = "update accounts set balance = balance + ? where acc_number = ?";
+                    PreparedStatement preparedStatement1 = conn.prepareStatement(credit_query);
+                    preparedStatement1.setDouble(1,amount);
+                    preparedStatement1.setLong(2,acc_number);
+
+                    int affectedRows = preparedStatement.executeUpdate();
+
+                    if (affectedRows > 0){
+                        System.out.println("Rs. "+amount+" credited Successfully");
+                        conn.commit();
+                        conn.setAutoCommit(true);
+                        return;
+                    }
+                    else {
+                        System.out.println("TRANSACTION FAILED !");
+                        conn.rollback();
+                        conn.setAutoCommit(true);
+                    }
+
+                }
+                else {
+                    System.out.println("Invalid Security PIN ......");
+                }
+            }
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+        }
+        conn.setAutoCommit(true);
+    }
 }
